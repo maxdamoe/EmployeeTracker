1950fb667bab786de29fe8c2b96fe880c17aef93
msimp <max.simpson@gmail.com>
2021-08-11 18:43:22 -0500

    first commit

---
+++ db/db.sql
@@ -0,0 +1,20 @@
+
+CREATE TABLE Department (
+  id INTEGER PRIMARY KEY,
+  name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE Role (
+  id INTEGER PRIMARY KEY,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INTEGER,
+);
+
+CREATE TABLE Employee (
+ id INTEGER PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER,
+  manager_id INTEGER,
+);
